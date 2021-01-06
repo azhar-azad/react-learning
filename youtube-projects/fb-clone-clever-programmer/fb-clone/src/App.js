db6d38814da09056ea405026b0ae3b9672e0147f
azhar-azad <azhar.u.azad@gmail.com>
2021-01-06 21:48:51 +0600

    firebase auth and Login comp added

--- youtube-projects/fb-clone-clever-programmer/fb-clone/src/App.js
+++ youtube-projects/fb-clone-clever-programmer/fb-clone/src/App.js
@@ -1,22 +1,31 @@
+import React from 'react';
 import './App.css';
+import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
 import Widgets from './components/Widgets/Widgets';
 
 function App() {
+  const user = null;
+  
   return (
     // BEM naming convention
     <div className="app">
       
-      {/*Header*/}
-      <Header />
-      
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div>
+          <Header />
+    
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </div>
+      )}
       
       
     </div>
--- youtube-projects/fb-clone-clever-programmer/fb-clone/src/firebase.js
+++ youtube-projects/fb-clone-clever-programmer/fb-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA0BfNAhIu9EONYHCCuOyabIf0whOgJiZY",
@@ -8,3 +10,13 @@ const firebaseConfig = {
   appId: "1:45174060671:web:1ae14b68efebe9b2d7b14a",
   measurementId: "G-YVH8LZWR0L"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth;
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+export { auth, provider };
+export default db;
